f0ea7d8cbec2a617444363012d85df49ea399185
Sally Mclean <smclean17@gmail.com>
2021-04-27 16:52:10 +0200

    set lastN based on clientHeight and layout

--- react/features/base/lastn/middleware.ts
+++ react/features/base/lastn/middleware.ts
@@ -4,6 +4,7 @@ import { IStore } from '../../app/types';
 import { SET_FILMSTRIP_ENABLED } from '../../filmstrip/actionTypes';
 import { SELECT_LARGE_VIDEO_PARTICIPANT } from '../../large-video/actionTypes';
 import { APP_STATE_CHANGED } from '../../mobile/background/actionTypes';
+import { LAYOUTS, getCurrentLayout, getTileViewGridDimensions, shouldDisplayTileView } from '../../video-layout';
 import {
     SCREEN_SHARE_REMOTE_PARTICIPANTS_UPDATED,
     SET_CAR_MODE,
@@ -21,7 +22,10 @@ import {
     getParticipantById,
     getParticipantCount
 } from '../participants/functions';
-import MiddlewareRegistry from '../redux/MiddlewareRegistry';
+import { MiddlewareRegistry } from '../redux';
+import {
+    CLIENT_RESIZED
+} from '../responsive-ui/actionTypes';
 import { isLocalVideoTrackDesktop } from '../tracks/functions';
 
 import { setLastN } from './actions';
@@ -96,7 +100,10 @@ const _updateLastN = debounce(({ dispatch, getState }: IStore) => {
 MiddlewareRegistry.register(store => next => action => {
     const result = next(action);
 
+
+    // sally - set lastn on client resise
     switch (action.type) {
+    case CLIENT_RESIZED:
     case APP_STATE_CHANGED:
     case CONFERENCE_JOINED:
     case PARTICIPANT_JOINED:
@@ -115,3 +122,83 @@ MiddlewareRegistry.register(store => next => action => {
 
     return result;
 });
+
+/**
+ * Updates the last N value in the conference based on the current state of the redux store.
+ *
+ * @param {Store} store - The redux store.
+ * @private
+ * @returns {void}
+ */
+function _updateLastN({ getState }) {
+    const state = getState();
+    const { conference } = state['features/base/conference'];
+    const { enabled: audioOnly } = state['features/base/audio-only'];
+    const { appState } = state['features/background'] || {};
+    const { enabled: filmStripEnabled } = state['features/filmstrip'];
+    const config = state['features/base/config'];
+    const { lastNLimits } = state['features/base/lastn'];
+    const participantCount = getParticipantCount(state);
+
+
+    // sally
+    const layout = getCurrentLayout(state);
+    const { clientHeight } = state['features/base/responsive-ui'];
+
+    if (!conference) {
+        logger.debug('There is no active conference, not updating last N');
+
+        return;
+    }
+
+    let lastN = typeof config.channelLastN === 'undefined' ? -1 : config.channelLastN;
+
+    // Apply last N limit based on the # of participants and channelLastN settings.
+    const limitedLastN = limitLastN(participantCount, lastNLimits);
+
+    if (limitedLastN !== undefined) {
+        lastN = lastN === -1 ? limitedLastN : Math.min(limitedLastN, lastN);
+    }
+
+    // sally  - hard code lastn to 8 for Tile View
+    if (layout === LAYOUTS.TILE_VIEW) {
+        lastN = 8;
+    } else {
+        // dynamically set lastN when not in tile view based on client height
+        lastN = Math.round(clientHeight / 200) + 1;
+    }
+
+    if (typeof appState !== 'undefined' && appState !== 'active') {
+        lastN = isLocalVideoTrackDesktop(state) ? 1 : 0;
+    } else if (audioOnly) {
+        const { remoteScreenShares, tileViewEnabled } = state['features/video-layout'];
+        const largeVideoParticipantId = state['features/large-video'].participantId;
+        const largeVideoParticipant
+            = largeVideoParticipantId ? getParticipantById(state, largeVideoParticipantId) : undefined;
+
+        // Use tileViewEnabled state from redux here instead of determining if client should be in tile
+        // view since we make an exception only for screenshare when in audio-only mode. If the user unpins
+        // the screenshare, lastN will be set to 0 here. It will be set to 1 if screenshare has been auto pinned.
+        if (!tileViewEnabled && largeVideoParticipant && !largeVideoParticipant.local) {
+            lastN = (remoteScreenShares || []).includes(largeVideoParticipantId) ? 1 : 0;
+        } else {
+            lastN = 0;
+        }
+    } else if (!filmStripEnabled) {
+        lastN = 1;
+    }
+
+    if (conference.getLastN() === lastN) {
+        return;
+    }
+
+    logger.info(`Setting last N to: ${lastN}`);
+
+    try {
+        console.log('SETTING LASTN');
+        console.log(lastN);
+        conference.setLastN(lastN);
+    } catch (err) {
+        logger.error(`Failed to set lastN: ${err}`);
+    }
+}
