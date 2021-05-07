a78e0bbb557a14ebb87d6787b9d33bf9aa16fdb4
Sally Mclean <smclean17@gmail.com>
2021-05-07 13:12:18 +0200

    fix for encoded apostrophe

--- react/features/display-name/components/web/DisplayName.tsx
+++ react/features/display-name/components/web/DisplayName.tsx
@@ -331,10 +331,13 @@ class DisplayName extends Component<Props, State> {
 function _mapStateToProps(state: IState, ownProps: Partial<Props>) {
     const { participantID } = ownProps;
     const participant = getParticipantById(state, participantID ?? '');
+    let participantName = getParticipantDisplayName(state, participantID ?? '');
+
+    participantName = participantName.replace(/&#39;/g, '\'');
 
     return {
         _configuredDisplayName: participant?.name,
-        _nameToDisplay: getParticipantDisplayName(state, participantID ?? '')
+        _nameToDisplay: participantName
     };
 }
 
