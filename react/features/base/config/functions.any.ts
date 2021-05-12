0e4035974e73091bf54ab7fe3e95d9a648a9a725
Sally Mclean <smclean17@gmail.com>
2021-05-12 15:17:33 +0200

    remove tileview

--- react/features/base/config/functions.any.ts
+++ react/features/base/config/functions.any.ts
@@ -24,7 +24,7 @@ var configOverride = {
     startWithAudioMuted: true,
     startWithVideoMuted: true,
     toolbarButtons: [
-       'microphone', 'camera', 'desktop', 'profile', 'settings', 'filmstrip', 'tileview'
+       'microphone', 'camera', 'desktop', 'profile', 'settings', 'filmstrip'
     ]
 }
 // XXX The function getRoomName is split out of
