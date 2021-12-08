65a3d4a4a55d73de203cf161ef0c95b6a7787a18
Nemo Grippa <nemo.grippa@gmail.com>
2021-12-08 16:21:06 +0100

    Disable mobile app promotion

--- interface_config.js
+++ interface_config.js
@@ -115,7 +115,7 @@ var interfaceConfig = {
      *
      * @type {boolean}
      */
-    MOBILE_APP_PROMO: true,
+    MOBILE_APP_PROMO: false,
 
     /**
      * Specify custom URL for downloading android mobile app.
