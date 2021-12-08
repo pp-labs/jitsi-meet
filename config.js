81cfa0f2eaeaf44e3f2ca109b5cdaaff2c6eab1f
Nemo Grippa <nemo.grippa@gmail.com>
2021-12-08 16:18:42 +0100

    Disable moderator message

--- config.js
+++ config.js
@@ -1053,67 +1053,67 @@ var config = {
 
         A falsy value for this prop will result in having all notifications enabled (e.g null, undefined, false)
     */
-    // notifications: [
-    //     'connection.CONNFAIL', // shown when the connection fails,
-    //     'dialog.cameraNotSendingData', // shown when there's no feed from user's camera
-    //     'dialog.kickTitle', // shown when user has been kicked
-    //     'dialog.liveStreaming', // livestreaming notifications (pending, on, off, limits)
-    //     'dialog.lockTitle', // shown when setting conference password fails
-    //     'dialog.maxUsersLimitReached', // shown when maximmum users limit has been reached
-    //     'dialog.micNotSendingData', // shown when user's mic is not sending any audio
-    //     'dialog.passwordNotSupportedTitle', // shown when setting conference password fails due to password format
-    //     'dialog.recording', // recording notifications (pending, on, off, limits)
-    //     'dialog.remoteControlTitle', // remote control notifications (allowed, denied, start, stop, error)
-    //     'dialog.reservationError',
-    //     'dialog.serviceUnavailable', // shown when server is not reachable
-    //     'dialog.sessTerminated', // shown when there is a failed conference session
-    //     'dialog.sessionRestarted', // show when a client reload is initiated because of bridge migration
-    //     'dialog.tokenAuthFailed', // show when an invalid jwt is used
-    //     'dialog.transcribing', // transcribing notifications (pending, off)
-    //     'dialOut.statusMessage', // shown when dial out status is updated.
-    //     'liveStreaming.busy', // shown when livestreaming service is busy
-    //     'liveStreaming.failedToStart', // shown when livestreaming fails to start
-    //     'liveStreaming.unavailableTitle', // shown when livestreaming service is not reachable
-    //     'lobby.joinRejectedMessage', // shown when while in a lobby, user's request to join is rejected
-    //     'lobby.notificationTitle', // shown when lobby is toggled and when join requests are allowed / denied
-    //     'localRecording.localRecording', // shown when a local recording is started
-    //     'notify.disconnected', // shown when a participant has left
-    //     'notify.connectedOneMember', // show when a participant joined
-    //     'notify.connectedTwoMembers', // show when two participants joined simultaneously
-    //     'notify.connectedThreePlusMembers', // show when more than 2 participants joined simultaneously
-    //     'notify.grantedTo', // shown when moderator rights were granted to a participant
-    //     'notify.invitedOneMember', // shown when 1 participant has been invited
-    //     'notify.invitedThreePlusMembers', // shown when 3+ participants have been invited
-    //     'notify.invitedTwoMembers', // shown when 2 participants have been invited
-    //     'notify.kickParticipant', // shown when a participant is kicked
-    //     'notify.moderationStartedTitle', // shown when AV moderation is activated
-    //     'notify.moderationStoppedTitle', // shown when AV moderation is deactivated
-    //     'notify.moderationInEffectTitle', // shown when user attempts to unmute audio during AV moderation
-    //     'notify.moderationInEffectVideoTitle', // shown when user attempts to enable video during AV moderation
-    //     'notify.moderationInEffectCSTitle', // shown when user attempts to share content during AV moderation
-    //     'notify.mutedRemotelyTitle', // shown when user is muted by a remote party
-    //     'notify.mutedTitle', // shown when user has been muted upon joining,
-    //     'notify.newDeviceAudioTitle', // prompts the user to use a newly detected audio device
-    //     'notify.newDeviceCameraTitle', // prompts the user to use a newly detected camera
-    //     'notify.passwordRemovedRemotely', // shown when a password has been removed remotely
-    //     'notify.passwordSetRemotely', // shown when a password has been set remotely
-    //     'notify.raisedHand', // shown when a partcipant used raise hand,
-    //     'notify.startSilentTitle', // shown when user joined with no audio
-    //     'notify.unmute', // shown to moderator when user raises hand during AV moderation
-    //     'prejoin.errorDialOut',
-    //     'prejoin.errorDialOutDisconnected',
-    //     'prejoin.errorDialOutFailed',
-    //     'prejoin.errorDialOutStatus',
-    //     'prejoin.errorStatusCode',
-    //     'prejoin.errorValidation',
-    //     'recording.busy', // shown when recording service is busy
-    //     'recording.failedToStart', // shown when recording fails to start
-    //     'recording.unavailableTitle', // shown when recording service is not reachable
-    //     'toolbar.noAudioSignalTitle', // shown when a broken mic is detected
-    //     'toolbar.noisyAudioInputTitle', // shown when noise is detected for the current microphone
-    //     'toolbar.talkWhileMutedPopup', // shown when user tries to speak while muted
-    //     'transcribing.failedToStart' // shown when transcribing fails to start
-    // ],
+    notifications: [
+        'connection.CONNFAIL', // shown when the connection fails,
+        'dialog.cameraNotSendingData', // shown when there's no feed from user's camera
+        'dialog.kickTitle', // shown when user has been kicked
+        'dialog.liveStreaming', // livestreaming notifications (pending, on, off, limits)
+        'dialog.lockTitle', // shown when setting conference password fails
+        'dialog.maxUsersLimitReached', // shown when maximmum users limit has been reached
+        'dialog.micNotSendingData', // shown when user's mic is not sending any audio
+        'dialog.passwordNotSupportedTitle', // shown when setting conference password fails due to password format
+        'dialog.recording', // recording notifications (pending, on, off, limits)
+        'dialog.remoteControlTitle', // remote control notifications (allowed, denied, start, stop, error)
+        'dialog.reservationError',
+        'dialog.serviceUnavailable', // shown when server is not reachable
+        'dialog.sessTerminated', // shown when there is a failed conference session
+        'dialog.sessionRestarted', // show when a client reload is initiated because of bridge migration
+        'dialog.tokenAuthFailed', // show when an invalid jwt is used
+        'dialog.transcribing', // transcribing notifications (pending, off)
+        'dialOut.statusMessage', // shown when dial out status is updated.
+        'liveStreaming.busy', // shown when livestreaming service is busy
+        'liveStreaming.failedToStart', // shown when livestreaming fails to start
+        'liveStreaming.unavailableTitle', // shown when livestreaming service is not reachable
+        'lobby.joinRejectedMessage', // shown when while in a lobby, user's request to join is rejected
+        'lobby.notificationTitle', // shown when lobby is toggled and when join requests are allowed / denied
+        'localRecording.localRecording', // shown when a local recording is started
+        'notify.disconnected', // shown when a participant has left
+        'notify.connectedOneMember', // show when a participant joined
+        'notify.connectedTwoMembers', // show when two participants joined simultaneously
+        'notify.connectedThreePlusMembers', // show when more than 2 participants joined simultaneously
+        // 'notify.grantedTo', // shown when moderator rights were granted to a participant
+        'notify.invitedOneMember', // shown when 1 participant has been invited
+        'notify.invitedThreePlusMembers', // shown when 3+ participants have been invited
+        'notify.invitedTwoMembers', // shown when 2 participants have been invited
+        'notify.kickParticipant', // shown when a participant is kicked
+        'notify.moderationStartedTitle', // shown when AV moderation is activated
+        'notify.moderationStoppedTitle', // shown when AV moderation is deactivated
+        'notify.moderationInEffectTitle', // shown when user attempts to unmute audio during AV moderation
+        'notify.moderationInEffectVideoTitle', // shown when user attempts to enable video during AV moderation
+        'notify.moderationInEffectCSTitle', // shown when user attempts to share content during AV moderation
+        'notify.mutedRemotelyTitle', // shown when user is muted by a remote party
+        'notify.mutedTitle', // shown when user has been muted upon joining,
+        'notify.newDeviceAudioTitle', // prompts the user to use a newly detected audio device
+        'notify.newDeviceCameraTitle', // prompts the user to use a newly detected camera
+        'notify.passwordRemovedRemotely', // shown when a password has been removed remotely
+        'notify.passwordSetRemotely', // shown when a password has been set remotely
+        'notify.raisedHand', // shown when a partcipant used raise hand,
+        'notify.startSilentTitle', // shown when user joined with no audio
+        'notify.unmute', // shown to moderator when user raises hand during AV moderation
+        'prejoin.errorDialOut',
+        'prejoin.errorDialOutDisconnected',
+        'prejoin.errorDialOutFailed',
+        'prejoin.errorDialOutStatus',
+        'prejoin.errorStatusCode',
+        'prejoin.errorValidation',
+        'recording.busy', // shown when recording service is busy
+        'recording.failedToStart', // shown when recording fails to start
+        'recording.unavailableTitle', // shown when recording service is not reachable
+        'toolbar.noAudioSignalTitle', // shown when a broken mic is detected
+        'toolbar.noisyAudioInputTitle', // shown when noise is detected for the current microphone
+        'toolbar.talkWhileMutedPopup', // shown when user tries to speak while muted
+        'transcribing.failedToStart' // shown when transcribing fails to start
+    ],
 
     // Prevent the filmstrip from autohiding when screen width is under a certain threshold
     // disableFilmstripAutohiding: false,
