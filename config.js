19ba30e7a473f8474c15fa58880d11e8d7a72972
njunghans <junghans.nino@googlemail.com>
2022-10-17 12:34:17 +0200

    remove example domains

--- config.js
+++ config.js
@@ -45,14 +45,14 @@ export var config = {
         // focus: 'focus.jitsi-meet.example.com',
 
         // XMPP MUC domain. FIXME: use XEP-0030 to discover it.
-        muc: "conference." + subdomain + "jitsi-meet.example.com",
+        // muc: "conference." + subdomain + "jitsi-meet.example.com",
     },
 
     // BOSH URL. FIXME: use XEP-0156 to discover it.
-    bosh: "//jitsi-meet.example.com/" + subdir + "http-bind",
+    // bosh: "//jitsi-meet.example.com/" + subdir + "http-bind",
 
     // Websocket URL
-    websocket: "wss://jitsi-meet.example.com/" + subdir + "xmpp-websocket",
+    // websocket: "wss://jitsi-meet.example.com/" + subdir + "xmpp-websocket",
 
     // The real JID of focus participant - can be overridden here
     // Do not change username - FIXME: Make focus username configurable
